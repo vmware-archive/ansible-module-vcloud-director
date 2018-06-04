16cfdbe17137770cb56bcc9fc1e4ed3ca1c14aca
mukultaneja <mtaneja@vmware.com>
2018-06-04 15:30:29 +0530

    WIP vCloud Director VDC Module Signed-off-by: Mukul Taneja <mtaneja@mtaneja-a01.vmware.com>

---
+++ modules/vcd_org_vdc.py
@@ -0,0 +1,372 @@
+# Copyright © 2018 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+# !/usr/bin/python
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+client: vcd_org_vdc
+short_description: This module is to manage virtual datacenters for organizations
+version_added: "2.4"
+description:
+    - "This module is to manage virtual datacenters for organizations"
+options:
+    user:
+        description:
+            - vCloud Director user name
+        required: false
+    password:
+        description:
+            - vCloud Director user password
+        required: false
+    host:
+        description:
+            - vCloud Director host address
+        required: false
+    org:
+        description:
+            - Organization name on vCloud Director to access
+        required: false
+    api_version:
+        description:
+            - Pyvcloud API version
+        required: false
+    verify_ssl_certs:
+        description:
+            - whether to use secure connection to vCloud Director host
+        required: false
+    name:
+        description:
+            - The name of the new vdc
+        required: false
+    provider_vdc_name:
+        description:
+            - The name of an existing provider vdc
+        required: false
+    description:
+        description:
+            - The description of the new org vdc
+        required: false
+    allocation_model:
+        description:
+            - The allocation model used by this vDC. One of AllocationVApp, AllocationPool or ReservationPool
+        required: false
+    cpu_units:
+        description:
+            - The cpu units compute capacity allocated to this vDC. One of MHz or GHz
+        required: false
+    cpu_allocated:
+        description:
+            - Capacity that is committed to be available
+        required: false
+    cpu_limit:
+        description:
+            - Capacity limit relative to the value specified for Allocation
+        required: false
+    mem_units:
+        description:
+            - The memory units compute capacity allocated to this vDC. One of MB or GB
+        required: false
+    mem_allocated:
+        description:
+            - Memory capacity that is committed to be available
+        required: false
+    mem_limit:
+        description:
+            - Memory capacity limit relative to the value specified for Allocation
+        required: false
+    nic_quota:
+        description:
+            - Maximum number of virtual NICs allowed in this vDC. Defaults to 0, which specifies an unlimited number
+        required: false
+    network_quota:
+        description:
+            - Maximum number of network objects that can be deployed in this vDC. Defaults to 0, which means no networks can be deployed
+        required: false
+    vm_quota:
+        description:
+            - The maximum number of VMs that can be created in this vDC. Defaults to 0, which specifies an unlimited number
+        required: false
+    storage_profiles:
+        description:
+            - List of provider vDC storage profiles to add to this vDC. Each item is a dictionary that should include the following elements
+                - name: name of the PVDC storage profile
+                - enabled: True if the storage profile is enabled for this vDC else false
+                - units: Units used to define limit. One of MB or GB
+                - limit: Max number of units allocated for this storage profile
+                - default: True if this is default storage profile for this vDC
+        required: false
+    resource_guaranteed_memory:
+        description:
+            - Percentage of allocated CPU resources guaranteed to vApps deployed in this vDC. Value defaults to 1.0 if the element is empty
+        required: false
+    resource_guaranteed_cpu:
+        description:
+            - Percentage of allocated memory resources guaranteed to vApps deployed in this vDC. Value defaults to 1.0 if the element is empty
+        required: false
+    vcpu_in_mhz:
+        description:
+            - Specifies the clock frequency, in Megahertz, for any virtual CPU that is allocated to a VM
+        required: false
+    is_thin_provision:
+        description:
+            - Boolean to request thin provisioning
+        required: false
+    network_pool_name:
+        description:
+            - Reference to a network pool in the Provider vDC
+        required: false
+    uses_fast_provisioning:
+        description:
+            - Boolean to request fast provisioning
+        required: false
+    over_commit_allowed:
+        description:
+            - Set to false to disallow creation of the VDC if the AllocationModel is AllocationPool or ReservationPool and the ComputeCapacity you specified is greater than what the backing Provider VDC can supply. Defaults to true if empty or missing
+        required: false
+    vm_discovery_enabled:
+        description:
+            - True if discovery of vCenter VMs is enabled for resource pools backing this vDC
+        required: false
+    is_enabled:
+        description:
+            - True if this vDC is enabled for use by the organization users
+        required: false
+    state:
+        description:
+            - state of new virtual datacenter ('present'/'absent').
+            - One from state or operation has to be provided.
+        required: false
+    operation:
+        description:
+            - operations performed over new vdc ('update').
+            - One from state or operation has to be provided.
+        required: false
+
+author:
+    - mtaneja@vmware.com
+'''
+
+EXAMPLES = '''
+- name: Test with a message
+  vcd_org_vdc:
+    user: terraform
+    password: abcd
+    host: csa.sandbox.org
+    org: Terraform
+    api_version: 30
+    verify_ssl_certs: False
+    name = "VDC_NAME"
+    provider_vdc_name = "PVDC_NAME"
+    description = "DESCRIPTION"
+    allocation_model = "AllocationVApp"
+    cpu_units = "MHz"
+    cpu_allocated = "0"
+    cpu_limit = "0"
+    mem_units = "MB"
+    mem_allocated = "0"
+    mem_limit = "0"
+    nic_quota = "0"
+    network_quota = "0"
+    vm_quota = "0"
+    storage_profiles = "PF1,PF1"
+    resource_guaranteed_memory = "None"
+    resource_guaranteed_cpu = "None"
+    vcpu_in_mhz = "None"
+    is_thin_provision = "None"
+    network_pool_name = "None"
+    uses_fast_provisioning = "None"
+    over_commit_allowed = "None"
+    vm_discovery_enabled = "None"
+    is_enabled = "true"
+    state = "present"
+'''
+
+RETURN = '''
+result: success/failure message relates to vapp_org_vdc operation
+'''
+
+import json
+from pyvcloud.vcd.org import Org
+from pyvcloud.vcd.vdc import VDC
+from lxml import objectify, etree
+from pyvcloud.vcd.system import System
+from ansible.module_utils.vcd import VcdAnsibleModule
+
+
+ORG_VDC_STATES = ['present', 'absent']
+ORG_VDC_OPERATIONS = ['update']
+
+
+def org_vdc_argument_spec():
+    return dict(
+        name=dict(type='str', required=False),
+        provider_vdc_name=dict(type='str', required=False),
+        description=dict(type='str', required=False),
+        allocation_model=dict(type='str', required=False),
+        cpu_units=dict(type='str', required=False),
+        cpu_allocated=dict(type='str', required=False),
+        cpu_limit=dict(type='str', required=False),
+        mem_units=dict(type='str', required=False),
+        mem_allocated=dict(type='str', required=False),
+        mem_limit=dict(type='str', required=False),
+        nic_quota=dict(type='str', required=False),
+        network_quota=dict(type='str', required=False),
+        vm_quota=dict(type='str', required=False),
+        storage_profiles=dict(type='dict', required=False),
+        resource_guaranteed_memory=dict(type='str', required=False),
+        resource_guaranteed_cpu=dict(type='str', required=False),
+        vcpu_in_mhz=dict(type='str', required=False),
+        is_thin_provision=dict(type='str', required=False),
+        network_pool_name=dict(type='str', required=False),
+        uses_fast_provisioning=dict(type='str', required=False),
+        over_commit_allowed=dict(type='str', required=False),
+        vm_discovery_enabled=dict(type='str', required=False),
+        is_enabled=dict(type='str', required=False),
+        state=dict(choices=ORG_VDC_STATES, required=False),
+        operation=dict(choices=ORG_VDC_OPERATIONS, required=False)
+    )
+
+
+class Vdc(VcdAnsibleModule):
+    def __init__(self, **kwargs):
+        super(Vdc, self).__init__(**   kwargs)
+
+    def get_org(self):
+        logged_in_org = self.client.get_org()
+
+        return Org(self.client, resource=logged_in_org)
+
+    def create(self):
+        name = self.params.get('name')
+        is_enabled = self.params.get('is_enabled')
+        provider_vdc_name = self.params.get('provider_vdc_name')
+        description = self.params.get('description')
+        allocation_model = self.params.get('allocation_model')
+        # storage_profiles = self.params.get('storage_profiles')
+        cpu_units = self.params.get('cpu_units')
+        cpu_allocated = self.params.get('cpu_allocated')
+        cpu_limit = self.params.get('cpu_limit')
+        mem_units = self.params.get('mem_units')
+        mem_allocated = self.params.get('mem_allocated')
+        mem_limit = self.params.get('mem_limit')
+        nic_quota = self.params.get('nic_quota')
+        network_quota = self.params.get('network_quota')
+        vm_quota = self.params.get('vm_quota')
+        resource_guaranteed_memory = self.params.get('resource_guaranteed_memory')
+        resource_guaranteed_cpu = self.params.get('resource_guaranteed_cpu')
+        vcpu_in_mhz = self.params.get('vcpu_in_mhz')
+        is_thin_provision = self.params.get('is_thin_provision')
+        network_pool_name = self.params.get('network_pool_name')
+        uses_fast_provisioning = self.params.get('uses_fast_provisioning')
+        over_commit_allowed = self.params.get('over_commit_allowed')
+        vm_discovery_enabled = self.params.get('vm_discovery_enabled')
+        # storage_profiles = json.loads(storage_profiles)
+        response = dict()
+
+        if not network_pool_name.strip():
+            network_pool_name = None
+
+        org = self.get_org()
+        create_vdc_task = org.create_org_vdc(
+            vdc_name=name,
+            provider_vdc_name=provider_vdc_name,
+            description=description,
+            allocation_model=allocation_model,
+            # storage_profiles=storage_profiles,
+            cpu_units=cpu_units,
+            cpu_allocated=cpu_allocated,
+            cpu_limit=cpu_limit,
+            mem_units=mem_units,
+            mem_allocated=mem_allocated,
+            mem_limit=mem_limit,
+            nic_quota=nic_quota,
+            network_quota=network_quota,
+            vm_quota=vm_quota,
+            resource_guaranteed_memory=resource_guaranteed_memory,
+            resource_guaranteed_cpu=resource_guaranteed_cpu,
+            vcpu_in_mhz=vcpu_in_mhz,
+            is_thin_provision=is_thin_provision,
+            network_pool_name=network_pool_name,
+            uses_fast_provisioning=uses_fast_provisioning,
+            over_commit_allowed=over_commit_allowed,
+            vm_discovery_enabled=vm_discovery_enabled,
+            is_enabled=is_enabled)
+
+        self.execute_task(create_vdc_task)
+        response['msg'] = 'VDC {} has been created.'.format(name)
+        response['changed'] = True
+
+        return response
+
+    def update(self):
+        org = self.get_org()
+        name = self.params.get('name')
+        is_enabled = self.params.get('is_enabled')
+        response = dict()
+
+        vdc_resource = org.get_vdc(name)
+        vdc = VDC(self.client, name=name, resource=vdc_resource)
+        vdc.enable_vdc(is_enabled)
+        response['msg'] = 'VDC {} has been updated'.format(name)
+        response['changed'] = True
+
+        return response
+
+    def delete(self):
+        org = self.get_org()
+        name = self.params.get('name')
+
+        vdc_resource = org.get_vdc(name)
+        vdc = VDC(self.client, name=name, resource=vdc_resource)
+        delete_vdc_task = vdc.delete_vdc()
+        self.execute_task(delete_vdc_task)
+        response['msg'] = 'VDC {} has been deleted.'.format(name)
+        response['changed'] = True
+
+        return response
+
+    def manage_states(self):
+        state = self.params.get('state')
+        if state == 'present':
+            self.create()
+
+        if state == 'absent':
+            self.delete()
+
+    def manage_operations(self):
+        operation = self.params.get('operation')
+        if operation == 'update':
+            self.update()
+
+
+def main():
+    argument_spec = org_vdc_argument_spec()
+    response = dict(
+        msg=dict(type='str')
+    )
+
+    module = Vdc(argument_spec=argument_spec, supports_check_mode=True)
+
+    try:
+        if module.params.get('state'):
+            module.manage_states()
+        elif module.params.get('operation'):
+            module.manage_operations()
+        else:
+            raise Exception('One of from state/operation should be provided.')
+    except Exception as error:
+        response['msg'] = error.__str__()
+        module.fail_json(**response)
+
+    module.exit_json(**response)
+
+
+if __name__ == '__main__':
+    main()
