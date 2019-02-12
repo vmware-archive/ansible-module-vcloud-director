9c74847e19664a43a4c476ee8f15b8d6a5c2bba8
Chaminda Divitotawela <cdivitotawela@australiacloud.com.au>
2019-02-12 15:23:30 +1000

    Removed a text which breaks vcd_org_vdc.py module which seems to have been added accidently

--- modules/vcd_org_vdc.py
+++ modules/vcd_org_vdc.py
@@ -1,4 +1,4 @@
-required: false# Copyright © 2018 VMware, Inc. All Rights Reserved.
+# Copyright © 2018 VMware, Inc. All Rights Reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 
 # !/usr/bin/python
