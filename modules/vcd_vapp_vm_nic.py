4e25d1a3fd1617a7b2172825d1b0ebe6afa03e87
Sebastien Lacoste-Seris <sebastien@lacoste-seris.net>
2019-05-29 19:58:54 +0200

    Fixed module documentation for update action
    The update action is incorrectly documented as a parameter for the operation parameter while the code expects it in the state parameter.
    
    Signed-off-by: Sebastien Lacoste-Seris <sebastien@lacoste-seris.net>

--- modules/vcd_vapp_vm_nic.py
+++ modules/vcd_vapp_vm_nic.py
@@ -1,5 +1,5 @@
 # Copyright © 2018 VMware, Inc. All Rights Reserved.
-# SPDX-License-Identifier: BSD-2-Clause OR GPL-3.0-only
+# SPDX-License-Identifier: BSD-2-Clause
 
 # !/usr/bin/python
 
@@ -11,11 +11,11 @@ ANSIBLE_METADATA = {
 
 DOCUMENTATION = '''
 ---
-module: vcd_vapp_vm_nic
-short_description: Ansible Module to manage (create/delete/update) NICs in vApp VMs in vCloud Director.
+module: vcd_vapp_vm_disk
+short_description: Ansible Module to manage (create/update/delete) Disks in vApp VMs in vCloud Director.
 version_added: "2.4"
 description:
-    - "Ansible Module to manage (create/delete/update) NICs in vApp VMs."
+    - "Ansible Module to manage (create/update/delete) Disks in vApp VMs."
 
 options:
     user:
@@ -42,17 +42,13 @@ options:
         description:
             - whether to use secure connection to vCloud Director host
         required: false
-    nic_id:
+    disk_name:
         description:
-            - NIC ID
+            - Disk Name
         required: false
-    nic_ids:
+    disks:
         description:
-            - List of NIC IDs
-        required: false
-    network:
-        description:
-            - VApp network name
+            - List of Disk Names
         required: false
     vm_name:
         description:
@@ -66,22 +62,18 @@ options:
         description:
             - VDC name
         required: true
-    ip_allocation_mode:
+    size:
         description:
-            - IP allocation mode (DHCP, POOL or MANUAL)
-        required: false
-    ip_address:
-        description:
-            - NIC IP address (required for MANUAL IP allocation mode)
+            - Disk size in MB
         required: false
     state:
         description:
-            - state of nic (present/absent).
+            - state of disk ('present'/'absent'/'update').
             - One from state or operation has to be provided.
         required: true
     operation:
         description:
-            - operation on nic (update/read).
+            - operation on Disk ('read').
             - One from state or operation has to be provided.
         required: false
 author:
@@ -90,7 +82,7 @@ author:
 
 EXAMPLES = '''
 - name: Test with a message
-  vcd_vapp_vm_nic:
+  vcd_vapp_vm_disk:
     user: terraform
     password: abcd
     host: csa.sandbox.org
@@ -100,78 +92,74 @@ EXAMPLES = '''
     vm: "vm1"
     vapp = "vapp1"
     vdc = "vdc1"
-    nic_id = "2000"
+    disk_name = "Hard disk 1"
+    size = "2147483648"
     state = "present"
 '''
 
 RETURN = '''
-msg: success/failure message corresponding to nic state
+msg: success/failure message corresponding to disk state
 changed: true if resource has been changed else false
 '''
 
-from copy import deepcopy
-from lxml import etree
-from pyvcloud.vcd.vm import VM
+import math
 from pyvcloud.vcd.org import Org
 from pyvcloud.vcd.vdc import VDC
-from pyvcloud.vcd.client import E
 from pyvcloud.vcd.vapp import VApp
-from collections import defaultdict
-from pyvcloud.vcd.client import E_RASD
-from pyvcloud.vcd.client import NSMAP
+from pyvcloud.vcd.vm import VM
 from pyvcloud.vcd.client import EntityType
+from pyvcloud.vcd.client import NSMAP
 from ansible.module_utils.vcd import VcdAnsibleModule
-from pyvcloud.vcd.exceptions import EntityNotFoundException, OperationNotSupportedException
+from pyvcloud.vcd.exceptions import EntityNotFoundException
 
 
-VAPP_VM_NIC_STATES = ['present', 'absent', 'update']
-VAPP_VM_NIC_OPERATIONS = ['read']
+VAPP_VM_DISK_STATES = ['present', 'absent', 'update']
+VAPP_VM_DISK_OPERATIONS = ['read']
 
 
-def vapp_vm_nic_argument_spec():
+def vapp_vm_disk_argument_spec():
     return dict(
         vm_name=dict(type='str', required=True),
         vapp=dict(type='str', required=True),
         vdc=dict(type='str', required=True),
-        nic_id=dict(type='int', required=False),
-        nic_ids=dict(type='list', required=False),
-        ip_allocation_mode=dict(type='str', required=False),
-        ip_address=dict(type='str', required=False,),
-        network=dict(type='str', required=False),
-        state=dict(choices=VAPP_VM_NIC_STATES, required=False),
-        operation=dict(choices=VAPP_VM_NIC_OPERATIONS, required=False),
+        disk_name=dict(type='str', required=False),
+        disks=dict(type='list', required=False),
+        size=dict(type='int', required=False),
+        state=dict(choices=VAPP_VM_DISK_STATES, required=False),
+        operation=dict(choices=VAPP_VM_DISK_OPERATIONS, required=False),
     )
 
 
-class VappVMNIC(VcdAnsibleModule):
+class VappVMDisk(VcdAnsibleModule):
     def __init__(self, **kwargs):
-        super(VappVMNIC, self).__init__(**kwargs)
+        super(VappVMDisk, self).__init__(**kwargs)
         vapp_resource = self.get_resource()
         self.vapp = VApp(self.client, resource=vapp_resource)
 
     def manage_states(self):
         state = self.params.get('state')
         if state == "present":
-            return self.add_nic()
+            return self.add_disk()
 
         if state == "update":
-            return self.update_nic()
+            return self.update_disk()
 
         if state == "absent":
-            return self.delete_nic()
+            return self.delete_disk()
 
     def manage_operations(self):
         operation = self.params.get('operation')
 
         if operation == "read":
-            return self.read_nics()
+            return self.read_disks()
 
     def get_resource(self):
         vapp = self.params.get('vapp')
         vdc = self.params.get('vdc')
         org_resource = Org(self.client, resource=self.client.get_org())
         vdc_resource = VDC(self.client, resource=org_resource.get_vdc(vdc))
-        vapp_resource_href = vdc_resource.get_resource_href(name=vapp, entity_type=EntityType.VAPP)
+        vapp_resource_href = vdc_resource.get_resource_href(
+            name=vapp, entity_type=EntityType.VAPP)
         vapp_resource = self.client.get_resource(vapp_resource_href)
 
         return vapp_resource
@@ -181,168 +169,129 @@ class VappVMNIC(VcdAnsibleModule):
 
         return VM(self.client, resource=vapp_vm_resource)
 
-    def get_vm_nics(self):
-        vm = self.get_vm()
-
-        return self.client.get_resource(vm.resource.get('href') + '/networkConnectionSection')
-
-    def add_nic(self):
-        '''
-            Error - More than 10 Nics are not permissible in vCD
-        '''
-        vm = self.get_vm()
-        network = self.params.get('network')
-        ip_address = self.params.get('ip_address')
-        ip_allocation_mode = self.params.get('ip_allocation_mode')
-        uri = vm.resource.get('href') + '/networkConnectionSection'
-        response = defaultdict(dict)
+    def add_disk(self):
+        size = self.params.get('size')
+        vm_name = self.params.get('vm_name')
+        response = dict()
         response['changed'] = False
-        new_nic_id = None
 
-        nics = self.get_vm_nics()
-        nics_indexes = [int(nic.NetworkConnectionIndex) for nic in nics.NetworkConnection]
-        nics_indexes.sort()
-        for index, nic_index in enumerate(nics_indexes):
-            new_nic_id = nic_index + 1
-            if index != nic_index:
-                new_nic_id = index
-                break
-
-        if ip_allocation_mode not in ('DHCP', 'POOL', 'MANUAL'):
-            raise Exception('IpAllocationMode should be one of DHCP/POOL/MANUAL')
-
-        if ip_allocation_mode in ('DHCP', 'POOL'):
-            nic = E.NetworkConnection(
-                E.NetworkConnectionIndex(new_nic_id),
-                E.IsConnected(True),
-                E.IpAddressAllocationMode(ip_allocation_mode),
-                network=network)
-        else:
-            if not ip_address:
-                raise Exception('IpAddress is missing.')
-            nic = E.NetworkConnection(
-                E.NetworkConnectionIndex(new_nic_id),
-                E.IpAddress(ip_address),
-                E.IsConnected(True),
-                E.IpAddressAllocationMode(ip_allocation_mode),
-                network=network)
-
-        nics.NetworkConnection.addnext(nic)
-        add_nic_task = self.client.put_resource(uri, nics, EntityType.NETWORK_CONNECTION_SECTION.value)
-        self.execute_task(add_nic_task)
-        response['msg'] = {
-            'nic_id': new_nic_id,
-            'ip_allocation_mode': ip_allocation_mode,
-            'ip_address': ip_address
+        disks = self.read_disks().get('disks')
+        last_disk = list(disks.keys())[-1]
+        last_disk_instance_id = int(disks[last_disk]['InstanceID'])
+        new_disk_name = str(int(last_disk.split(' ')[2]) + 1)
+        new_disk_instance_id = last_disk_instance_id + 1
+        add_disk_task = self.vapp.add_disk_to_vm(vm_name, size)
+        self.execute_task(add_disk_task)
+        msg = 'Vapp VM disk of size {0} has been added.'
+        response['msg'] = msg.format(str(size))
+        response['metadata'] = {
+            'new_disk_name': 'Hard disk {0}'.format(new_disk_name),
+            'new_disk_size': size,
+            'new_disk_instance_id': new_disk_instance_id
         }
         response['changed'] = True
 
         return response
 
-    def update_nic(self):
-        '''
-            Following update scenarios are covered
-            1. MANUAL mode to DHCP
-            2. Update IP address in MANUAL mode
-            3. DHCP to MANUAL mode
-        '''
+    def read_disks(self):
         vm = self.get_vm()
-        nic_id = self.params.get('nic_id')
-        network = self.params.get('network')
-        ip_address = self.params.get('ip_address')
-        ip_allocation_mode = self.params.get('ip_allocation_mode')
-        uri = vm.resource.get('href') + '/networkConnectionSection'
-        response = defaultdict(dict)
+        response = dict()
         response['changed'] = False
-
-        nics = self.get_vm_nics()
-        nic_indexs = [nic.NetworkConnectionIndex for nic in nics.NetworkConnection]
-        if nic_id not in nic_indexs:
-            raise EntityNotFoundException('Can\'t find the specified VM nic')
-        nic_to_update = nic_indexs.index(nic_id)
-
-        if network:
-            nics.NetworkConnection[nic_to_update].set('network', network)
-            response['changed'] = True
-
-        if ip_allocation_mode:
-            allocation_mode_element = E.IpAddressAllocationMode(ip_allocation_mode)
-            nics.NetworkConnection[nic_to_update].IpAddressAllocationMode = allocation_mode_element
-            response['changed'] = True
-
-        if ip_address:
-            response['changed'] = True
-            if hasattr(nics.NetworkConnection[nic_to_update], 'IpAddress'):
-                nics.NetworkConnection[nic_to_update].IpAddress = E.IpAddress(ip_address)
-            else:
-                network = nics.NetworkConnection[nic_to_update].get('network')
-                nics.NetworkConnection[nic_to_update] = E.NetworkConnection(
-                    E.NetworkConnectionIndex(nic_id),
-                    E.IpAddress(ip_address),
-                    E.IsConnected(True),
-                    E.IpAddressAllocationMode(ip_allocation_mode),
-                    network=network)
-
-        if response['changed']:
-            update_nic_task = self.client.put_resource(uri, nics, EntityType.NETWORK_CONNECTION_SECTION.value)
-            self.execute_task(update_nic_task)
-            response['msg'] = 'Vapp VM nic has been updated.'
+        response['disks'] = dict()
+        disks = self.client.get_resource(vm.resource.get('href') + '/virtualHardwareSection/disks')
+        for disk in disks.Item:
+            if disk['{' + NSMAP['rasd'] + '}Description'] == "Hard disk":
+                disk_name = str(disk['{' + NSMAP['rasd'] + '}ElementName'])
+                disk_instance = int(disk['{' + NSMAP['rasd'] + '}InstanceID'])
+                disk_size = int(disk['{' + NSMAP['rasd'] + '}VirtualQuantity'])
+                disk_hostresource = disk['{' + NSMAP['rasd'] + '}HostResource']
+                disk_capacity = int(disk_hostresource.get('{' + NSMAP['vcloud'] + '}capacity'))
+                response['disks'][disk_name] = {
+                    'InstanceID': disk_instance,
+                    'VirtualQuantity': self.convert_bytes_to_gb(disk_size),
+                    'HostResource': str(round(disk_capacity / 1024, 1)) + ' GB'
+                }
 
         return response
 
-    def read_nics(self):
-        response = defaultdict(dict)
+    def convert_bytes_to_gb(self, disk_size):
+        log_value = int(math.floor(math.log(disk_size, 1024)))
+        pow_value = math.pow(1024, log_value)
+
+        return str(round(disk_size / pow_value, 1)) + ' GB'
+
+    def update_disk(self):
+        vm = self.get_vm()
+        disk_name = self.params.get('disk_name')
+        size = self.params.get('size')
+        response = dict()
         response['changed'] = False
+        index = -1
 
-        nics = self.get_vm_nics()
-        for nic in nics.NetworkConnection:
-            meta = defaultdict(dict)
-            nic_id = str(nic.NetworkConnectionIndex)
-            meta['MACAddress'] = str(nic.MACAddress)
-            meta['IsConnected'] = str(nic.IsConnected)
-            meta['NetworkAdapterType'] = str(nic.NetworkAdapterType)
-            meta['NetworkConnectionIndex'] = str(nic.NetworkConnectionIndex)
-            meta['IpAddressAllocationMode'] = str(nic.IpAddressAllocationMode)
+        if not size:
+            err = '''Hard disk size argument is missing. Disk \'update\' operation only allows disk size to be updated
+            for the exisiting VM\'s.'''
+            raise Exception(err)
 
-            if hasattr(nic, 'IpAddress'):
-                meta['IpAddress'] = str(nic.IpAddress)
+        disks = self.client.get_resource(vm.resource.get('href') + '/virtualHardwareSection/disks')
+        for i, disk in enumerate(disks.Item):
+            if disk['{' + NSMAP['rasd'] + '}ElementName'] == disk_name:
+                index = i
 
-            response['msg'][nic_id] = meta
+        if index < 0:
+            err = 'Can\'t find the specified VM disk with name {0}'
+            err = err.format(disk_name)
+            raise EntityNotFoundException(err)
+
+        disks.Item[index]['{' + NSMAP['rasd'] + '}VirtualQuantity'] = size * 1024 * 1024
+        disks.Item[index]['{' + NSMAP['rasd'] + '}HostResource'].set(
+            '{' + NSMAP['vcloud'] + '}capacity', str(size))
+        update_disk_task = self.client.put_resource(
+            vm.resource.get('href') +
+            '/virtualHardwareSection/disks', disks, EntityType.RASD_ITEMS_LIST.value)
+        self.execute_task(update_disk_task)
+
+        msg = 'Vapp VM disk with name {0} has been updated.'
+        response['msg'] = msg.format(disk_name)
+        response['changed'] = True
 
         return response
 
-    def delete_nic(self):
+    def delete_disk(self):
         vm = self.get_vm()
-        nic_ids = self.params.get('nic_ids')
-        response = defaultdict(dict)
+        disks_to_remove = self.params.get('disks')
+        response = dict()
         response['changed'] = False
-        uri = vm.resource.get('href') + '/networkConnectionSection'
 
-        nics = self.get_vm_nics()
-        for nic in nics.NetworkConnection:
-            if nic.NetworkConnectionIndex in nic_ids:
-                nics.remove(nic)
-                nic_ids.remove(nic.NetworkConnectionIndex)
+        disks = self.client.get_resource(vm.resource.get('href') + '/virtualHardwareSection/disks')
 
-        if len(nic_ids) > 0:
-            nic_ids = [str(nic_id) for nic_id in nic_ids]
-            err_msg = 'Can\'t find the specified VM nic(s) {0}'.format(','.join(nic_ids))
-            raise EntityNotFoundException(err_msg)
+        for disk in disks.Item:
+            if disk['{' + NSMAP['rasd'] + '}ElementName'] in disks_to_remove:
+                disks.remove(disk)
+                disks_to_remove.remove(disk['{' + NSMAP['rasd'] + '}ElementName'])
 
-        remove_nic_task = self.client.put_resource(uri, nics, EntityType.NETWORK_CONNECTION_SECTION.value)
-        self.execute_task(remove_nic_task)
-        response['msg'] = 'VM nic(s) has been deleted.'
+        if len(disks_to_remove) > 0:
+            err = 'VM disk(s) with name {0} was not found.'
+            err = err.format(','.join(disks_to_remove))
+
+            raise EntityNotFoundException(err)
+
+        remove_disk_task = self.client.put_resource(vm.resource.get(
+            'href') + '/virtualHardwareSection/disks',
+            disks, EntityType.RASD_ITEMS_LIST.value)
+        self.execute_task(remove_disk_task)
+        response['msg'] = 'VM disk(s) has been deleted.'
         response['changed'] = True
 
         return response
 
 
 def main():
-    argument_spec = vapp_vm_nic_argument_spec()
+    argument_spec = vapp_vm_disk_argument_spec()
     response = dict(
         msg=dict(type='str')
     )
-    module = VappVMNIC(argument_spec=argument_spec, supports_check_mode=True)
+    module = VappVMDisk(argument_spec=argument_spec, supports_check_mode=True)
 
     try:
         if module.params.get('state'):
@@ -354,6 +303,7 @@ def main():
 
     except Exception as error:
         response['msg'] = error
+        response['changed'] = False
         module.fail_json(**response)
 
     module.exit_json(**response)
