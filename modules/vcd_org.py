411b595c732e1cadabea9eaaf4e1d60d6cce0412
Jan Gaßner <jan.gassner@plusserver.com>
2019-10-24 09:59:20 +0200

    Add operations list_users, list_vdcs, set_metadata
    Operation list_users returns key "users" with dicts of users as per pyvclouds org.list_users() and utils.to_dict()
    Operation list_vdcs returns key "vdcs" with dicts of vdcs as per pyvclouds org.list_vdcs()
    Operation set_metadata expects key "metadata" and sets metadata on org. Example-Data:
    metadata:
      - state: 'present'                                             # optional, present (will also update value), absent, default: present
        name: 'keyX'                                                 # mandatory, unique name of metadata-entry
        value: 'valueY'                                              # mandatory, if state present, else optional as it will be ignored
        type: 'STRING'                                               # optional, STRING, NUMBER, BOOLEAN, DATA_TIME, default: STRING
        visibility: 'READONLY'                                       # optional, PRIVATE, READONLY, READ_WRITE, default: READONLY
    
    Signed-off-by: Jan Gaßner <jan.gassner@plusserver.com>

--- modules/vcd_org.py
+++ modules/vcd_org.py
@@ -62,6 +62,17 @@ options:
         description:
             - recursive delete org
         required: false
+    metadata:
+        description:
+            - dict to set, delete or update metadata
+            - Example:
+              metadata:
+                - state: 'present'                                             # optional, present (will also update value), absent, default: present
+                  name: 'keyX'                                                 # mandatory, unique name of metadata-entry
+                  value: 'valueY'                                              # mandatory, if state present, else optional as it will be ignored
+                  type: 'STRING'                                               # optional, STRING, NUMBER, BOOLEAN, DATA_TIME, default: STRING
+                  visibility: 'READONLY'                                       # optional, PRIVATE, READONLY, READ_WRITE, default: READONLY
+        required: false
     state:
         description:
             - state of org
@@ -104,11 +115,13 @@ changed: true if resource has been changed else false
 from lxml import etree
 from pyvcloud.vcd.org import Org
 from pyvcloud.vcd.system import System
+from pyvcloud.vcd.client import E, EntityType, RelationType, ResourceType, MetadataDomain, MetadataVisibility, MetadataValueType
+from pyvcloud.vcd.utils import to_dict
 from ansible.module_utils.vcd import VcdAnsibleModule
 from pyvcloud.vcd.exceptions import EntityNotFoundException, BadRequestException
 
 VCD_ORG_STATES = ['present', 'absent', 'update']
-VCD_ORG_OPERATIONS = ['read']
+VCD_ORG_OPERATIONS = ['read','list_users','list_vdcs','set_metadata']
 
 
 def org_argument_spec():
@@ -118,6 +131,7 @@ def org_argument_spec():
         is_enabled=dict(type='bool', required=False, default=False),
         force=dict(type='bool', required=False, default=None),
         recursive=dict(type='bool', required=False, default=None),
+        metadata=dict(type='list', required=False, default='[]'),
         state=dict(choices=VCD_ORG_STATES, required=False),
         operation=dict(choices=VCD_ORG_OPERATIONS, required=False),
     )
@@ -144,11 +158,18 @@ class VCDOrg(VcdAnsibleModule):
         operation = self.params.get('operation')
         if operation == "read":
             return self.read()
+        if operation == "list_users":
+            return self.list_users()
+        if operation == "list_vdcs":
+            return self.list_vdcs()
+        if operation == "set_metadata":
+            return self.set_metadata()
 
     def create(self):
         org_name = self.params.get('org_name')
         full_name = self.params.get('full_name')
         is_enabled = self.params.get('is_enabled')
+        metadata = self.params.get('metadata')
         response = dict()
         response['changed'] = False
 
@@ -208,6 +229,80 @@ class VCDOrg(VcdAnsibleModule):
 
         return response
 
+    def list_users(self):
+        org_name = self.params.get('org_name')
+        response = dict()
+        org_details = dict()
+        response['users'] = list()
+        response['changed'] = False
+
+        resource = self.client.get_org_by_name(org_name)
+        org = Org(self.client, resource=resource)
+        org_user_list = org.list_users()
+        resource_type = ResourceType.USER.value
+        if self.client.is_sysadmin():
+            resource_type = ResourceType.ADMIN_USER.value
+        for org_user in org_user_list:
+            response['users'].append(
+                to_dict(org_user, resource_type=resource_type, exclude=[]))
+
+        return response
+    
+    def list_vdcs(self):
+        org_name = self.params.get('org_name')
+        response = dict()
+        org_details = dict()
+        response['vdcs'] = list()
+        response['changed'] = False
+
+        resource = self.client.get_org_by_name(org_name)
+        org = Org(self.client, resource=resource)
+        response['vdcs'] = org.list_vdcs()
+
+        return response
+    
+    def set_metadata(self):
+        '''
+        Expects metadata as following:
+        metadata:
+          - state: 'present'                                             # optional, present (will also update value), absent, default: present
+            name: 'keyX'                                                 # mandatory, unique name of metadata-entry
+            value: 'valueY'                                              # mandatory, if state present, else optional as it will be ignored
+            type: 'STRING'                                               # optional, STRING, NUMBER, BOOLEAN, DATA_TIME, default: STRING
+            visibility: 'READONLY'                                       # optional, PRIVATE, READONLY, READ_WRITE, default: READONLY
+        '''
+        org_name = self.params.get('org_name')
+        metadata = self.params.get('metadata')
+        response = dict()
+        response['msg'] = ''
+        
+        if len(metadata) != 0:
+            # workaround to set metadata for org as it is as of now not implemented in pyvcloud for org, vdc, e.g. - we will open a pull request to fix this in the future
+            resource = self.client.get_linked_resource(self.client.get_org_by_name(org_name), RelationType.DOWN, EntityType.METADATA.value)
+            self.metadata = Metadata(self.client, resource=resource)
+            for md in metadata:
+                domain = MetadataDomain.SYSTEM
+                visibility = MetadataVisibility.READONLY
+                if type(md) is dict and md.get('state', 'present') == 'absent':
+                    if md.get('visibility', 'READONLY').upper() == 'READWRITE':
+                        domain = MetadataDomain.GENERAL
+                    self.metadata.remove_metadata()
+                else:
+                    if md.get('visibility', 'READONLY').upper() == 'PRIVATE':
+                        visibility = MetadataVisibility.PRIVATE
+                    elif md.get('visibility', 'READONLY').upper() == 'READWRITE':
+                        domain = MetadataDomain.GENERAL
+                        visibility = MetadataVisibility.READWRITE
+                    value_type = MetadataValueType.STRING
+                    if md.get('type', 'STRING').upper() == 'NUMBER':
+                        value_type = MetadataValueType.NUMBER
+                    elif md.get('type', 'STRING').upper() == 'BOOLEAN':
+                        value_type = MetadataValueType.BOOLEAN
+                    elif md.get('type', 'STRING').upper() == 'DATA_TIME':
+                        value_type = MetadataValueType.DATA_TIME
+                    self.metadata.set_metadata(md['name'], md['value'], domain, visibility, value_type, True)
+        
+        return response
 
 def main():
     argument_spec = org_argument_spec()
